f1fb88d640b7137b34d68388de419af51a9abda5
TonyGregg <antonygenil@gmail.com>
2018-07-04 20:33:36 -0400

    Added some more JPA methods at PersonRepository. Added logger to PersonController

--- src/main/java/com/genil/microservices/itsme360/contollers/PersonController.java
+++ src/main/java/com/genil/microservices/itsme360/contollers/PersonController.java
@@ -2,6 +2,8 @@ package com.genil.microservices.itsme360.contollers;
 
 import com.genil.microservices.itsme360.model.Person;
 import com.genil.microservices.itsme360.repositories.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/persons")
 public class PersonController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     PersonRepository personRepository;
@@ -28,12 +31,29 @@ public class PersonController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody Person person) {
+        logger.info("Person info passed "+person);
         personRepository.save(person);
+        logger.info("Person persisted into DB !");
 
     }
 
     @GetMapping("/{id}")
     public Person get(@PathVariable("id") Long id) {
         return personRepository.getOne(id);
+    }
+
+    @GetMapping("/findByEmailId")
+    public Person findByEmail(@RequestParam String email) {
+        return personRepository.findByEmail(email);
+    }
+
+    @GetMapping("/findByName")
+    public Person findPersonByName(@RequestParam String name) {
+        return personRepository.findByName(name);
+    }
+
+    @GetMapping("/findByEmailAndName")
+    public Person findByEmailAndName(@RequestParam String email, @RequestParam String name) {
+        return personRepository.findByEmailAndName(email,name);
     }
 }
--- src/main/java/com/genil/microservices/itsme360/repositories/PersonRepository.java
+++ src/main/java/com/genil/microservices/itsme360/repositories/PersonRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by genil on 7/4/18 at 19 36
  **/
 public interface PersonRepository extends JpaRepository<Person,Long> {
+    public Person findByEmail(String email);
+    public Person findByName(String name);
+    public Person findByEmailAndName(String email, String name);
+
 }
