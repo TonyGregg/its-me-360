229a86af06098d49513aa26fce829090cc189cd0
TonyGregg <antonygenil@gmail.com>
2018-07-06 07:07:00 -0400

    Integrated with Angular 6 - Its-me-360 UI app

--- src/main/java/com/genil/microservices/itsme360/repositories/PersonRepository.java
+++ src/main/java/com/genil/microservices/itsme360/repositories/PersonRepository.java
@@ -2,10 +2,12 @@ package com.genil.microservices.itsme360.repositories;
 
 import com.genil.microservices.itsme360.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by genil on 7/4/18 at 19 36
  **/
+@Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {
     public Person findByEmail(String email);
     public Person findByName(String name);
